9c214b9ed80819c6ed37c0a6090d8f3eaeff3938
MarianaSilvaC <marscamargo123@gmail.com>
2023-09-22 13:53:33 -0700

    Primer Parcial Proyecto

---
+++ PaginaPersonal/js/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Selecciona la imagen del "Proyecto 1"
+    const proyecto1Imagen = document.querySelector('#imagen-pro1');
+
+    // Selecciona la ventana emergente del "Proyecto 1"
+    const proyecto1Ventana = document.querySelector('#proyecto1');
+
+    // Agrega un EventListener para el clic en la imagen
+    proyecto1Imagen.addEventListener('click', () => {
+        // Muestra la ventana emergente del "Proyecto 1"
+        proyecto1Ventana.style.display = 'block';
+    });
+
+    // Agrega un EventListener para el clic en la ventana emergente para cerrarla
+    proyecto1Ventana.addEventListener('click', () => {
+        // Oculta la ventana emergente del "Proyecto 1"
+        proyecto1Ventana.style.display = 'none';
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Selecciona la imagen del "Proyecto 2"
+    const proyecto1Imagen = document.querySelector('#imagen-pro2');
+
+    // Selecciona la ventana emergente del "Proyecto 2"
+    const proyecto1Ventana = document.querySelector('#proyecto2');
+
+    // Agrega un EventListener para el clic en la imagen
+    proyecto1Imagen.addEventListener('click', () => {
+        // Muestra la ventana emergente del "Proyecto 2"
+        proyecto1Ventana.style.display = 'block';
+    });
+
+    // Agrega un EventListener para el clic en la ventana emergente para cerrarla
+    proyecto1Ventana.addEventListener('click', () => {
+        // Oculta la ventana emergente del "Proyecto 1"
+        proyecto1Ventana.style.display = 'none';
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Selecciona la imagen del "Proyecto 2"
+    const proyecto1Imagen = document.querySelector('#imagen-pro3');
+
+    // Selecciona la ventana emergente del "Proyecto 2"
+    const proyecto1Ventana = document.querySelector('#proyecto3');
+
+    // Agrega un EventListener para el clic en la imagen
+    proyecto1Imagen.addEventListener('click', () => {
+        // Muestra la ventana emergente del "Proyecto 2"
+        proyecto1Ventana.style.display = 'block';
+    });
+
+    // Agrega un EventListener para el clic en la ventana emergente para cerrarla
+    proyecto1Ventana.addEventListener('click', () => {
+        // Oculta la ventana emergente del "Proyecto 1"
+        proyecto1Ventana.style.display = 'none';
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Selecciona la imagen del "Proyecto 2"
+    const proyecto1Imagen = document.querySelector('#imagen-pro4');
+
+    // Selecciona la ventana emergente del "Proyecto 2"
+    const proyecto1Ventana = document.querySelector('#proyecto4');
+
+    // Agrega un EventListener para el clic en la imagen
+    proyecto1Imagen.addEventListener('click', () => {
+        // Muestra la ventana emergente del "Proyecto 2"
+        proyecto1Ventana.style.display = 'block';
+    });
+
+    // Agrega un EventListener para el clic en la ventana emergente para cerrarla
+    proyecto1Ventana.addEventListener('click', () => {
+        // Oculta la ventana emergente del "Proyecto 1"
+        proyecto1Ventana.style.display = 'none';
+    });
+
+});
+
